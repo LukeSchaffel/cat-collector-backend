2e416e7f77756829172aaf60225dcb5e0a0e6e78
LukeSchaffel <lukeschaffel@gmail.com>
2022-05-05 13:54:08 -0400

    start

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 class Config:
   DEBUG = True
   SQLALCHEMY_ECHO = True
-  SQLALCHEMY_DATABASE_URI = DATABASE_URL
+  SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/cat_collector'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
